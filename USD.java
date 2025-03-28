3c02b89588074e66fb0ca079ae332e3877d828c6
saumyaaagarwal <saumyaagarwalwork@gmail.com>
2025-03-28 19:54:53 +0530

    Add files via upload

---
+++ USD.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class USD {
+public static void main(String args[]) {
+Scanner in = new Scanner(System.in);
+System.out.print("Enter amount in INR ");
+int num1 = in.nextInt();
+Double num2 = num1 / 85.47  ;
+System.out.print(" INR value converted to USD is : " + num2);
+}
+}
+
+
